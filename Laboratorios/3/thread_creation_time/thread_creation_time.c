29b0d7fda35278ee728fe392a3a92bc241feba64
mantofer <mantofer2000@gmail.com>
2020-09-05 19:45:56 -0700

    ya lo mide bien

--- Laboratorios/3/thread_creation_time/thread_creation_time.c
+++ Laboratorios/3/thread_creation_time/thread_creation_time.c
@@ -16,6 +16,9 @@ int main(int argc, char* arg[]){
 
     struct timespec before, after;
 
+    time_t fastestS;
+
+    long fastestNs;
 
     size_t test_count = 0;
 
@@ -34,7 +37,18 @@ int main(int argc, char* arg[]){
     // se empieza a medir el tiempo
     clock_gettime(CLOCK_MONOTONIC, &before);
     for (size_t i = 0; i < test_count; ++i) {
+        clock_gettime(CLOCK_MONOTONIC, &before);
         pthread_create(&threads[i], NULL, nullFunction, NULL);
+        clock_gettime(CLOCK_MONOTONIC, &after);
+            if(i == 0){
+                fastestS = after.tv_sec - before.tv_sec;
+                fastestNs = after.tv_nsec - before.tv_nsec;
+            }else{
+                if(fastestS + fastestNs > (after.tv_sec - before.tv_sec) + (after.tv_nsec - before.tv_nsec)){
+                    fastestS = after.tv_sec - before.tv_sec;
+                    fastestNs = after.tv_nsec - before.tv_nsec;
+                }
+            }
     }
 
     for (size_t i = 0; i < test_count; ++i) {
@@ -43,7 +57,7 @@ int main(int argc, char* arg[]){
     clock_gettime(CLOCK_MONOTONIC, &after);
 
 
-    printf("Thread time %ld s, %ld nanoseconds\n", after.tv_sec - before.tv_sec, after.tv_nsec - before.tv_nsec);
+    printf("Minimum thread creation and destruction time was %ld.%lds among %zd tests\n", fastestS, fastestNs, test_count);
     printf("Amount of tests : %zd\n", test_count);
     free(threads);
     return 0;
