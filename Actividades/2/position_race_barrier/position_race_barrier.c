62b5d483d8eacdd802289c4e4853c8cf41dc0fcd
mantofer2000 <mantofer2000@gmail.com>
2020-09-17 10:10:49 -0700

    Comment

--- Actividades/2/position_race_barrier/position_race_barrier.c
+++ Actividades/2/position_race_barrier/position_race_barrier.c
@@ -87,8 +87,8 @@ int main(int argc, char* arg[]) {
     pthread_mutex_destroy(&shared_data->mutex);
     pthread_barrier_destroy(&shared_data->barrier_start_race);
 
-    // Comment
-
+    // Comment 
+    // Merge Comment
     free(threads);
     free(shared_data);
     free(thread_data_list);
