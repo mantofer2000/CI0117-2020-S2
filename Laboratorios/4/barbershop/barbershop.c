a0e2b6d7e22b4982b7e4a127b590d6149f7704e8
mantofer2000 <mantofer2000@gmail.com>
2020-09-19 15:21:34 -0700

    Cambios

--- Laboratorios/4/barbershop/barbershop.c
+++ Laboratorios/4/barbershop/barbershop.c
@@ -6,16 +6,24 @@
 #include <semaphore.h>
 
 // Va a actuar de memoria compartida
-typedef struct 
-{
+typedef struct{
+    
     size_t num_customers;
     size_t num_waiting_room;
 
-
+    pthread_mutex_t mutex;
     sem_t customer_semaphore;
     sem_t barber_semaphore;
 
 }barber_shop_t; 
+
+typedef struct{
+
+    size_t customer_id;
+    barber_shop_t* barber_shop;
+
+}customer_t;
+
 
 
 // Metodos propios para el cliente
@@ -32,12 +40,39 @@ int barber_leaves();
 void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds);
 
 
-int main(){
+int main(int argc, char* arg[]) {
+
+	srand( time(NULL) );
+    size_t num_customers = 0;
+
+    if (argc >= 2) {
+        num_customers = (size_t)strtoul(arg[1], NULL, 10);
+    } else {
+        fprintf(stderr, "Error, invalid number of parameters\n");
+        return 1;
+    }
+
+
     return 0;
 }
 
+int customer_arrives(size_t id){
+    printf("Customer number %zd has arrived\n", id);
+    return 1;
+}
 
 
+int customer_leaves_full(size_t id){
+    printf("Barbershop is full\n");
+    printf("Customer number %zd is leaving\n", id);
+    return 1;
+}
+
+
+int customer_leaves(size_t id){
+    printf("Customer number %zd is leaving\n", id);
+    return 1;
+}
 
 
 void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds){
