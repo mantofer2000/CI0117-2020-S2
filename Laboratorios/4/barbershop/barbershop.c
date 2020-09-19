0a0b04981eb507e0aeb0a2b65d6de4d15e5be7ee
mantofer2000 <mantofer2000@gmail.com>
2020-09-19 14:16:26 -0700

    Random Sleep

--- Laboratorios/4/barbershop/barbershop.c
+++ Laboratorios/4/barbershop/barbershop.c
@@ -1,6 +1,8 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include <semaphore.h>
 
 // Va a actuar de memoria compartida
@@ -18,16 +20,30 @@ typedef struct
 
 // Metodos propios para el cliente
 int customer_arrives(size_t id);
-int customer_leaves(size_t id){}
-int customer_leaves_full(size_t id){}
-
+int customer_leaves(size_t id);
+int customer_leaves_full(size_t id);
 int get_hair_cut();
 
 // Metodos propios para el barbero
 int cut_hair();
 int barber_leaves();
 
+// otros metodos
+void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds);
+
 
 int main(){
     return 0;
+}
+
+
+
+
+
+void random_sleep(useconds_t min_milliseconds, useconds_t max_milliseconds){
+	useconds_t duration = min_milliseconds;
+	useconds_t range = max_milliseconds - min_milliseconds;
+	if ( range > 0 )
+		duration += rand() % range;
+	usleep( 1000 * duration );
 }
