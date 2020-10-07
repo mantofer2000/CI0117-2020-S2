a7546ac85e7c9b980e629b47d37c168acb5761fa
mantofer <mantofer2000@gmail.com>
2020-10-07 01:10:07 -0600

    trabajando

---
+++ Examenes/1/dna_sequences/acid_counter.c
@@ -0,0 +1,15 @@
+#include <acid_counter.h>
+
+
+acid_counter_t * create_acid_counter(size_t dna_id, size_t thread_id, dna_sequence_t* shared_data){
+    acid_counter_t * ptr = calloc(); 
+}
+
+int destroy_acid_counter(acid_counter_t *  private_data){
+
+}
+
+void* process_dna(void* args){
+    return NULL;
+}
+
--- Examenes/1/dna_sequences/dna_sequence.h
+++ Examenes/1/dna_sequences/dna_sequence.h
@@ -1,23 +1,32 @@
 #ifndef DNA_SEQUENCE_H
 #define DNA_SEQUENCE_H
 
-#define SEQUENCE_1 abcdefgh
-#define SEQUENCE_2 zxdeffrsafdsagfsgsrtg
+#define DNA_SEQUENCE_1 "abcdefgh"
+#define DNA_SEQUENCE_2 "zxdeffrsafdsagfsgsrtg"
 
 #include <pthread.h>
 
 // van a haber 2 dna_sequence, uno por cada hilo de adn
 typedef struct dna_sequence
-{
+{    
     // sale mas barato tener 2 arreglos que 3 estructuras
     int ocurrence_array_1 ['z' - 'a'];
     int ocurrence_array_2 ['z' - 'a'];
     // Este tiene que venir definido desde el main,
     // es variable compartida
+    // desde el main se puede referenciar
+    
     int common_array['z' - 'a'];
+
     // numero de hilos en TOTAl
     size_t thread_amount;
-    pthread_barrier_t barrier;
-    pthread_rwlock_t rwlock;
+    
+    pthread_rwlock_t rwlock_dna_1;
+    pthread_rwlock_t rwlock_dna_2;
+    pthread_rwlock_t rwlock_common;
 }dna_sequence_t;
+
+dna_sequence_t * create_dna_sequence(size_t thread_amount);
+int destroy_dna_sequence(dna_sequence_t * shared_data);
+
 #endif
