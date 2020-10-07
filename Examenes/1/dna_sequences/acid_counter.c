21696a9d56c81a288a4c92a30bcfa215f43a40f1
mantofer <mantofer2000@gmail.com>
2020-10-07 14:54:10 -0600

    woking

--- Examenes/1/dna_sequences/acid_counter.c
+++ Examenes/1/dna_sequences/acid_counter.c
@@ -1,14 +1,23 @@
 #include <acid_counter.h>
 
-
-acid_counter_t * create_acid_counter(size_t dna_id, size_t thread_id, dna_sequence_t* shared_data){
-    acid_counter_t * ptr = calloc(); 
+// falta validacion
+acid_counter_t * create_acid_counter(size_t thread_amount, size_t dna_id, size_t thread_id, dna_sequence_t* shared_data){
+    acid_counter_t * ptr = calloc(thread_amount, sizeof(acid_counter_t));
+    for(size_t i = 0; i < thread_amount, i++){
+        ptr[i].thread_id = i;
+        if(i < thread_amount/2){
+            ptr[i].dna_id = 1;
+        }else{
+            ptr[i].dna_id = 2;
+        }
+        ptr[i].shared_data = shared_data;
+    }
+    return ptr; 
 }
 
 int destroy_acid_counter(acid_counter_t *  private_data){
-
+    free(private_data);
 }
-
 void* process_dna(void* args){
     return NULL;
 }
