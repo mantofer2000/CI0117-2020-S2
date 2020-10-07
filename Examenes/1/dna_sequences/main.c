13514725f383a4c5a04c45fd09a4b2eac6719946
mantofer <mantofer2000@gmail.com>
2020-10-06 19:46:42 -0600

    empezando esta madre

---
+++ Examenes/1/dna_sequences/main.c
@@ -0,0 +1,11 @@
+#define _XOPEN_SOURCE 600
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]){
+    char * entrada = argv[1];
+    char * entrada2 = argv[2];
+    printf(entrada2);
+    return 0;
+}
