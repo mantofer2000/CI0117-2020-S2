f94b0800de78897464f1ab64d3e9d829f8d26682
mantofer <mantofer2000@gmail.com>
2020-10-07 18:57:52 -0600

    sad

--- Examenes/1/dna_sequences/acid_counter.c
+++ Examenes/1/dna_sequences/acid_counter.c
@@ -1,9 +1,10 @@
 #include <acid_counter.h>
 
 // falta validacion
+// asuntos de memoria
 acid_counter_t * create_acid_counter(size_t thread_amount, size_t dna_id, size_t thread_id, dna_sequence_t* shared_data){
     acid_counter_t * ptr = calloc(thread_amount, sizeof(acid_counter_t));
-    for(size_t i = 0; i < thread_amount, i++){
+    for(size_t i = 0; i < thread_amount; i++){
         ptr[i].thread_id = i;
         if(i < thread_amount/2){
             ptr[i].dna_id = 1;
@@ -18,6 +19,8 @@ acid_counter_t * create_acid_counter(size_t thread_amount, size_t dna_id, size_t
 int destroy_acid_counter(acid_counter_t *  private_data){
     free(private_data);
 }
+
+// falta implementarlo
 void* process_dna(void* args){
     return NULL;
 }
--- Examenes/1/dna_sequences/dna_sequence.h
+++ Examenes/1/dna_sequences/dna_sequence.h
@@ -3,6 +3,7 @@
 
 #define DNA_SEQUENCE_1 "abcdefgh"
 #define DNA_SEQUENCE_2 "zxdeffrsafdsagfsgsrtg"
+#define ARRAY_SIZE 'z' - 'a'
 
 #include <pthread.h>
 
@@ -10,19 +11,23 @@
 typedef struct dna_sequence
 {    
     // sale mas barato tener 2 arreglos que 3 estructuras
-    int ocurrence_array_1 ['z' - 'a'];
-    int ocurrence_array_2 ['z' - 'a'];
+    int ocurrence_array_1 [ARRAY_SIZE];
+    int ocurrence_array_2 [ARRAY_SIZE];
     // Este tiene que venir definido desde el main,
     // es variable compartida
     // desde el main se puede referenciar       
-    int common_array['z' - 'a'];
+    int common_array[ARRAY_SIZE];
 
     // numero de hilos en TOTAl
     size_t thread_amount;
     
-    pthread_rwlock_t rwlock_dna_1;
-    pthread_rwlock_t rwlock_dna_2;
-    pthread_rwlock_t rwlock_common;
+    // son arrays
+    pthread_barrier_t barrier;
+    pthread_rwlock_t * rwlock_common;
+    pthread_rwlock_t * rwlock_dna_1;
+    pthread_rwlock_t * rwlock_dna_2;
+
+    
 }dna_sequence_t;
 
 dna_sequence_t * create_dna_sequence(size_t thread_amount);
