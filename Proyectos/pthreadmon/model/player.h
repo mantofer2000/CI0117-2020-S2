dbb855341155a62d63ee835619ac756339d3c29f
gabrielrevillat <gabrielrevillat@yahoo.es>
2020-10-10 20:48:41 -0600

    Empezando player

---
+++ Proyectos/pthreadmon/model/player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define TOTAL_POKEMON 3
+
+#include "../model/pokemon.h"
+
+typedef struct player
+{
+    char* player_name;
+    // pokemon[3] pokemon_team;
+
+} player_t;
+
+
+#endif PLAYER_H
