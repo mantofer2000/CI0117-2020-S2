e6e5fcfda3796dcc071c1e2157d8566cecbf6dde
mantofer <mantofer2000@gmail.com>
2020-10-14 21:57:10 -0600

    MARCO : CAMBIAR NOMBRES

--- Proyectos/pthreadmon/controller/mainTest.c
+++ Proyectos/pthreadmon/controller/mainTest.c
@@ -10,10 +10,10 @@ int main(){
     // hay que corregir donde va esta vara
     // recordar hacer el main lo menos cargado posible
 
-    player_t * player_one = create_pokemon("Marco");
+    player_t * player_one = player_create("Marco");
     player_one->pokemon_team = choose_team();
 
-    player_t * player_two = create_pokemon("Gabriel");
+    player_t * player_two = player_create("Gabriel");
     player_two->pokemon_team = choose_team();
 
 
