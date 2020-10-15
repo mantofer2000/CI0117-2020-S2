ee13613d0d74c0561dbc0cfc0e9089732c962914
mantofer <mantofer2000@gmail.com>
2020-10-14 19:56:50 -0600

    workinkg

---
+++ Proyectos/pthreadmon/controller/mainTest.c
@@ -0,0 +1,17 @@
+
+// main tonto por ahora
+#include "../model/pokemon.h"
+
+int main(){
+    // poner el initialize de mapper.c
+
+    initialize_data();
+
+    pokemon_t * my_pokemon = create_pokemon(5);
+    printf("%s\n", my_pokemon->pokemon_info->speciesName);
+    
+    //destroy_pokemon(my_pokemon);
+    
+
+    return 0;
+}
