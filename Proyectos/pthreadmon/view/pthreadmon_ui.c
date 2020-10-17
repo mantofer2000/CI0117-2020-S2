a50a30d2352059d9800f03463a80cf2c6d28bdfc
gabrielrevillat <gabrielrevillat@yahoo.es>
2020-10-17 15:48:33 -0600

    Comenzando interfaz

--- Proyectos/pthreadmon/view/pthreadmon_ui.c
+++ Proyectos/pthreadmon/view/pthreadmon_ui.c
@@ -4,12 +4,28 @@
 GtkWidget *window; // Ventana.
 GtkWidget *grid;
 
+GtkWidget *button_start;
+
+static void start_clicked()
+{
+    g_print("Start button pressed\n");
+}
+
 static void activate(GtkApplication* app, gpointer user_data)
 {
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Pthreadmon");
     gtk_window_set_default_size(GTK_WINDOW(window), 350, 500);
     gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+    grid = gtk_grid_new();
+
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    button_start = gtk_button_new_with_label("Start");
+    g_signal_connect(button_start, "clicked", G_CALLBACK(start_clicked), NULL);
+
+    gtk_grid_attach(GTK_GRID(grid), button_start, 3, 9, 4, 2);
 
     gtk_widget_show_all(window);
 }
