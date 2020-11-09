e8ca195be27cd1fbbff7e709a81cf2a696808b7f
mantofer <mantofer2000@gmail.com>
2020-11-09 10:37:09 -0600

    schedule guided

---
+++ Laboratorios/7/schedule_guided/schedule_guided.cpp
@@ -0,0 +1,17 @@
+#include <omp.h>
+#include <bits/stdc++.h>
+
+int main(){
+
+    omp_set_num_threads(2);
+    #pragma omp parallel for schedule(guided, 2)
+    for(int i = 0; i < 20; i++){
+        #pragma omp critical
+        {
+            std :: cout << "Thread num : " << omp_get_thread_num() << " has iteration : " << i << std :: endl; 
+        }
+    }
+
+
+    return 0;
+}
