593bbc790d10ffd5dad5209b45ce0dd64fb3bc7b
mantofer <mantofer2000@gmail.com>
2020-11-10 21:35:33 -0600

    Modificando GItignore

--- Actividades/7/hello_mpi_ordered_for/hello_mpi_ordered_for.cpp
+++ Actividades/7/hello_mpi_ordered_for/hello_mpi_ordered_for.cpp
@@ -14,21 +14,23 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 
-    int message_sent, message_received;
 
-    
-
+    int message_received = 0;
     if(my_id != 0){
-        MPI_Recv(&message_received, 1 /* count*/, MPI_INT, my_id - 1 /*source*/, 123 /*message id*/, MPI_COMM_WORLD, &status);
-        message_received;
-        
+        MPI_Recv(&message_received, 1 /* count*/, MPI_INT, 0 /*source*/, 123 /*message id*/, MPI_COMM_WORLD, &status);
     }
-
     cout << "Hello. I am process: " << my_id << ". Total processes: " << num_processes << endl;
 
-    if(my_id < num_processes - 1){
-            MPI_Send(&message_received, 1 /*count*/, MPI_INT, my_id + 1 /*dest*/, 123 /*message id*/, MPI_COMM_WORLD);
+
+    if(my_id == 0){
+        int message_sent = 0;
+        for(int i = 1; i < num_processes; i++){
+            MPI_Send(&message_sent, 1 /*count*/, MPI_INT, i /*dest*/, 123 /*message id*/, MPI_COMM_WORLD);
+        }
     }
+
+
+
 
     MPI_Finalize();
 
