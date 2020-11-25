9fe85c515476611a36104f9d13b580cfc926eeec
mantofer <mantofer2000@gmail.com>
2020-11-25 12:18:55 -0600

    Operator Overload

--- Proyectos/super_mario_mpi/model/element.h
+++ Proyectos/super_mario_mpi/model/element.h
@@ -12,6 +12,10 @@
 class Element {
     public:
         virtual void action(Mario &) = 0;
+
+        bool operator==(const Element &other) {
+            return this->element_type == other.element_type;
+        };
     
     protected:
         std::string element_type;
