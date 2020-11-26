2b0d1027f58c7d237823ca8cc703b087a2d0601c
Gabriel <gabrielrevillat@yahoo.es>
2020-11-25 20:44:05 -0600

    World problemas

--- Proyectos/super_mario_mpi/controller/main.cpp
+++ Proyectos/super_mario_mpi/controller/main.cpp
@@ -1,10 +1,13 @@
 #include "../model/coin.h"
 #include "../model/goomba.h"
+#include "../model/world.h"
 
 int main() {
 
     Little_Goomba myGoomba;
     Coin myCoin;
+
+    World w;
 
     if (myCoin != myGoomba) {
         std::cout << "Diferrent Elements" << std::endl;
