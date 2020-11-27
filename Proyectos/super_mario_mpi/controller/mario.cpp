be6776a8642f0d88e6a44ec6c2288c87ce1cc3b8
mantofer <mantofer2000@gmail.com>
2020-11-27 00:13:04 -0600

    Mario Constructor Changes

--- Proyectos/super_mario_mpi/controller/mario.cpp
+++ Proyectos/super_mario_mpi/controller/mario.cpp
@@ -1,8 +1,41 @@
 #include "../model/mario.h"
 
+Mario::Mario(std::string attack_strategy) {
+    this->coins = 0;
+    this->active = true;
+    this->attack_strategy = attack_strategy;
+}
+
+
+
 Mario::Mario() {
     this->coins = 0;
     this->active = true;
+
+    srand (time(NULL));
+
+    int probability =  (rand() % 4) + 1;
+
+    switch (probability) {
+        case 1:
+            this->attack_strategy = R;
+            break;
+        
+        case 2:
+            this->attack_strategy = L_COIN;
+            break;
+        
+        case 3:
+            this->attack_strategy = M_COIN;
+            break;
+        
+        case 4:
+            this->attack_strategy = A;
+            break;
+
+        default:
+            this->attack_strategy = R;
+    }
 }
 
 void Mario::add_coin(int amount) {
