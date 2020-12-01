39354f1300fcd08615a8b6e1ed61adbe3cd8e7c2
gabrielrevillat <gabrielrevillat@yahoo.es>
2020-12-01 16:26:54 -0600

    Bcast?

--- Proyectos/super_mario_mpi/controller/main.cpp
+++ Proyectos/super_mario_mpi/controller/main.cpp
@@ -1,16 +1,24 @@
 #include "../model/world.h"
 #include "../model/element.h"
+#include <unistd.h>
 #include <mpi.h>
+
+bool remaining_still_alive(int* active_marios, int num_processes) {
+    for (int index = 0; index < num_processes - 1; ++index) {
+        if (active_marios[index] == 1) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main(int argc, char* argv[]) {
 
-    int my_id, num_processes, mario_status;
+    int my_id, num_processes, mario_status, players_alive;
     int *coin_array;
     int *active_marios;
     
     num_processes = 2;
-
-    active_marios = new int[num_processes - 1];
 
     // seed for the entire program
     srand (time(NULL));
@@ -23,17 +31,59 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 
     int player_to_view = 3;
-    char attack_strategy = '?';
+    char attack_strategy = 'R';
 
     // Hacer comprobacion de argumentos.
     /*if (argc >= 3) {
         player_to_view = std::stoi(argv[1]);
+
+        // Verificar que player_to_view no sea 0 ni mayor a la cantidad de procesos.
+        if (player_to_view == 0 || player_to_view >= num_processes) {
+            std::cerr << "Invalid player.\n";
+            return 3;
+        }
+
         attack_strategy = (char)argv[2];
+
+        if (attack_strategy != 'R' && attack_strategy != 'L'
+            && attack_strategy != 'M' && attack_strategy != 'A') {
+
+            std::cerr << "Attack strategies: R L M A\n";
+            return 2;
+
+        }
+
     } else {
         std::cerr << "Usage: super_mario_mpi player_to_view attack_strategy";
         return 1;
     }*/
-    // Verificar que player_to_view no sea 0 ni mayor a la cantidad de procesos.
+
+    // Son de tamano num_processes porque el allgather no puede ignorar al 0.
+    coin_array = new int[num_processes];
+
+    active_marios = new int[num_processes];
+    players_alive = 0;
+    for (int index = 1; index < num_processes; ++index) {
+        active_marios[index] = 1;
+    }
+
+    if (my_id == 0) {
+        mario_status = 0;
+        int coins = 0;
+        while (remaining_still_alive(active_marios, num_processes)) {
+
+            MPI_Bcast(&player_to_view, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Allreduce(&mario_status, &players_alive, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+            MPI_Allgather(&coins, 1, MPI_INT, coin_array, 1, MPI_INT, MPI_COMM_WORLD);
+            MPI_Allgather(&mario_status, 1, MPI_INT, active_marios, 1, MPI_INT, MPI_COMM_WORLD);
+
+            if (active_marios[player_to_view] == 0) {
+                std::cout << "Enter the number of another player: ";
+                std::cin >> player_to_view;
+            }
+        }
+    }
 
     if (my_id != 0) {
         
@@ -57,13 +107,26 @@ int main(int argc, char* argv[]) {
         Koopa_Troopa my_koopa;
 
         World my_world;
+
         Mario my_mario;
+
+        if (my_id == player_to_view) {
+            my_mario.set_attack_strategy(attack_strategy);
+        }
 
         int position = 0;
         
         while (my_mario.is_active()) {
 
+            mario_status = 1;
+            int coins = my_mario.get_coins_amount();
             // all gather vector monedas
+
+            MPI_Bcast(&player_to_view, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Allreduce(&mario_status, &players_alive, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+            MPI_Allgather(&coins, 1, MPI_INT, coin_array, 1, MPI_INT, MPI_COMM_WORLD);
+            MPI_Allgather(&mario_status, 1, MPI_INT, active_marios, 1, MPI_INT, MPI_COMM_WORLD);
 
             std::vector<Element*> world_position_elements = my_world.get_next_position_elements(position);
             int elements_count = world_position_elements.size();
@@ -74,9 +137,13 @@ int main(int argc, char* argv[]) {
 
                 std::cout << "World pos. " << position << ": ";
                 std::cout << "Mario #" << my_id << " is walking. ";
-                std::cout << "Coins: " << my_mario.get_coins_amount() << ' ';
+                std::cout << "Coins: " << my_mario.get_coins_amount() << " | ";
 
                 // Imprimir info de MPI: Attacking, Being attacked by, Attack Strategy, Total Playing.
+                std::cout << "attacking #- | ";
+                std::cout << "being attacked by #- | ";
+                std::cout << "attack strategy: " << my_mario.get_attack_strategy() << " | ";
+                std::cout << "Total playing: " << players_alive;
 
                 std::cout << '\n';
 
@@ -201,16 +268,29 @@ int main(int argc, char* argv[]) {
                 position++;
                 position = position % 100;
             } else {
+                mario_status = 0;
                 if (my_id == player_to_view) {
                     std::cout << "World pos. " << position << ": Mario #" << my_id << " Game Over.\n";
                 }
             }
+            // sleep(2);
             
         }
         
+        while (remaining_still_alive(active_marios, num_processes)) {
+            int coins = my_mario.get_coins_amount();
+
+            MPI_Bcast(&player_to_view, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Allreduce(&mario_status, &players_alive, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+            MPI_Allgather(&coins, 1, MPI_INT, coin_array, 1, MPI_INT, MPI_COMM_WORLD);
+            MPI_Allgather(&mario_status, 1, MPI_INT, active_marios, 1, MPI_INT, MPI_COMM_WORLD);
+
+        }
+
     }
 
-
+    
 
     MPI_Finalize();
 
